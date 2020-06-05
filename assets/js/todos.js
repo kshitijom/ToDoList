e098233e40591f96e4e4b06ef551da01be784ec8
kshitijom <mishrakshitij.646@gmail.com>
2020-06-05 19:02:26 +0530

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+//check off a todo on clicking
+$("ul").on("click", "li", function () {
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete
+$("ul").on("click", "span", function (event) {
+	$(this).parent().fadeOut(function () {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+//create a todo
+$("input[type='text']").on("keypress", function (event) {
+	if(event.which === 13) {
+		//grabbing new todo from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and append to ul
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>" + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").on("click", function () {
+	$("input[type='text']").fadeToggle();
+});
